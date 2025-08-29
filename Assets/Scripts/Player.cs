355becb06175e3c31d98fd44f81a258d8791fdec
aysegulisikoglu <aysegulisikoglu08@gmail.com>
2025-08-29 12:35:29 +0300

    a

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public FixedJoystick joystick;
+    public float moveSpeed;
+    float hInput, vInput;
+    int score = 0;
+
+    public GameObject winText;
+    public int winScore;
+
+    private void FixedUpdate()
+    {
+        hInput = joystick.Horizontal * moveSpeed;
+        vInput = joystick.Vertical * moveSpeed;
+
+        transform.Translate(hInput, vInput, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Carrot")
+        {
+            score++;
+            Destroy(collision.gameObject);
+            if (score >= winScore)
+            {
+                winText.SetActive(true);
+            }
+        }
+    }
+}
